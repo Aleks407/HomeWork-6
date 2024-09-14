2244b9248fabc0e0408ab84e50b8edead7d21968
Aleks <leksa407@gmail.com>
2024-09-14 13:02:16 +0300

    Домашнее задание №6 yна проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//Task 1
+        System.out.println("Задача 1");
+        for(int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+
+//Task 2
+        System.out.println("Задача 2");
+        for(int i=10; i>=1;i--){
+            System.out.println(i);
+        }
+//Task 3
+        System.out.println("Задача 3");
+        for(int i=0; i<=17;i=i+2){
+            System.out.println(i);
+        }
+//Task 4
+        System.out.println("Задача 4");
+        for(int i=10; i>=-10;i--){
+            System.out.println(i);
+        }
+//Task 5
+        System.out.println("Задача 5");
+        for(int i=1904; i<=2096;i=i+4){
+            System.out.println(i+" год является високосным");
+        }
+//Task 6
+        System.out.println("Задача 6");
+        for(int i=7; i<=98;i=i+7){
+            System.out.println(i);
+        }
+//Task 7
+        System.out.println("Задача 7");
+        for(int i=1; i<=512;i=i*2){
+            System.out.println(i);
+        }
+//Task 8
+        System.out.println("Задача 8");
+        int deposit= 29000;
+        int total=0;
+        for(int i=1; i<=12;i++){
+            total= deposit+total;
+            System.out.println("Месяц "+i+" сумма накоплений ровна "+total+" рублей");
+        }
+//Task 9
+        System.out.println("Задача 9");
+        int depositInBank= 29000;
+        int totalPlusPercent=0;
+        for(int i=1; i<=12;i++){
+            totalPlusPercent=totalPlusPercent+depositInBank+depositInBank/100;
+            System.out.println("Месяц "+i+" сумма накоплений ровна "+totalPlusPercent+" рублей");
+        }
+//Task 10
+        System.out.println("Задача 10 ");
+        int j=0;
+        for(int i=1;i<=10;i++) {
+            System.out.println("2*"+i+"="+2*i);
+        }
+
+
+
+
     }
 }
